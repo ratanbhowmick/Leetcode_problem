999e40db51b6b433677a07a491d9889c300c22c1
Ratan Bhowmick <106478004+ratanbhowmick@users.noreply.github.com>
2023-07-20 14:41:38 +0530

    Add files via upload

---
+++ Leedcode_Problem/string_compresion.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i=0;
+        int ansIndex=0;
+        int  n=chars.size();
+        
+        while(i<n){
+            int j=i+1;
+            while(j<n && chars[i]==chars[j]){
+                j++;
+            }
+            
+            chars[ansIndex++]=chars[i];
+            
+            int count= j-i;
+            
+            if(count>1){
+                
+                // converting counting into single digit and saving in answer
+                
+                string cnt= to_string(count);
+                for(char ch : cnt){
+                    chars[ansIndex++]=ch;
+                }
+            }
+            // moving to new/Differernt character
+            
+            i=j;
+        }
+        return ansIndex;
+    }
+};
